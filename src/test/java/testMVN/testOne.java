a7d4a67e87dabe12c3f0acd9fab24216ade79a06
Veeraprasad Therukalla <vptherukalla@valuelabs.com>
2017-02-03 10:24:05 +0530

    initial commit

---
+++ src/test/java/testMVN/testOne.java
@@ -0,0 +1,24 @@
+package testMVN;
+
+import org.testng.annotations.Test;
+
+public class testOne {
+	
+	//testNG annotations are: @Test, @BeforeSuite, @AfterSuite, @BeforeClass, @AfterClass, @BeforeMethod, @AfterMethod
+	
+		@Test(priority=2)
+	public void testLogout(){
+		
+		System.out.println("we are in Logout method calss 1");
+	}
+		
+		@Test(priority=1)
+		public void testLogin(){
+			
+			System.out.println("we are in Login method class 1");
+		}
+		
+
+	
+
+}
